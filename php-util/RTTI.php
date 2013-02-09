99828bca09f482b35f900272745e48f9d42d2fb6
thiagodp <thiago-dp@bol.com.br>
2013-02-09 04:24:02 -0200

    _

--- php-util/RTTI.php
+++ php-util/RTTI.php
@@ -1,7 +1,11 @@
 <?php
-/* THIS FILE IS DISTRIBUTED UNDER THE CREATIVE COMMONS LICENCE (CC BY 3.0)
- * http://creativecommons.org/licenses/by/3.0/
- * 
+/* THIS FILE:
+ * a)	BELONGS TO THE 'PHP-UTIL' LIBRARY:
+ *		https://github.com/thiagodp/php-util
+ *
+ * b)	IS DISTRIBUTED UNDER THE CREATIVE COMMONS LICENCE (CC BY 3.0):
+ * 		http://creativecommons.org/licenses/by/3.0/
+ *
  * USE IT AT YOUR OWN RISK!
  */
 
