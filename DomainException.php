7c4ea9af4fe2f98f0f39a071376f5f64adcf8ed0
thiagodp <thiago-dp@bol.com.br>
2013-11-12 02:37:09 -0200

    DomainException.php and RangeCheck.php added.

---
+++ DomainException.php
@@ -0,0 +1,19 @@
+<?php
+/* THIS FILE:
+ * a)	BELONGS TO THE 'PHP-UTIL' LIBRARY:
+ *		https://github.com/thiagodp/php-util
+ *
+ * b)	IS DISTRIBUTED UNDER THE CREATIVE COMMONS LICENCE (CC BY 3.0):
+ * 		http://creativecommons.org/licenses/by/3.0/
+ *
+ * USE IT AT YOUR OWN RISK!
+ */
+ 
+/**
+ * An exception related to the domain.
+ *
+ * @author	Thiago Delgado Pinto
+ */
+class DomainException extends RuntimeException {
+}
+?>
