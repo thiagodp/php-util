41953f65eeb79c4bf46d774ffb639ceed0cd670d
thiagodp <thiago-dp@bol.com.br>
2015-05-27 12:01:05 -0300

    @change HttpSender

--- HttpSender.php
+++ HttpSender.php
@@ -13,42 +13,66 @@
  * A simple CURL-less http sender.
  *
  * @author	Thiago Delgado Pinto
- * @version	0.9
+ * @version	1.0
  */
 class HttpSender {
 
 	/**
-	 * Send a POST request.
+	 * Send a HTTP POST request. This a convenience method for #send().
 	 *
-	 * @param url		the target url
-	 * @param content	the array with the content to send. Example: array( 'name' => 'Bob' )
-	 * @contentType		the content type. OPTIONAL, default 'application/x-www-form-urlencoded'.
-	 * @encodeURL		true for encoding the supplied url, false otherwise. OPTIONAL, default false.
-	 * @return			a string with the returning content or false in case of failure.
+	 * @see #send().
 	 */
-	function post( $url, array $content, $contentType='application/x-www-form-urlencoded',
-		$encodeURL = false
-		) {
+	function post( $url, array $content, $contentType='application/x-www-form-urlencoded', $encodeURL = false ) {
 		return $this->send( 'POST', $url, $content, $contentType, $encodeURL );
 	}
 	
 	/**
-	 * Send a GET request.
+	 * Send a HTTP GET request. This a convenience method for #send().
 	 *
-	 * @param url		the target url
-	 * @param content	the array with the content to send. Example: array( 'name' => 'Bob' )
-	 * @contentType		the content type. OPTIONAL, default 'application/x-www-form-urlencoded'.
-	 * @encodeURL		true for encoding the supplied url, false otherwise. OPTIONAL, default false.
-	 * @return			a string with the returning content or false in case of failure.
+	 * @see #send().
 	 */
-	function get( $url, array $content, $contentType='application/x-www-form-urlencoded',
-		$encodeURL = false
-		) {		
+	function get( $url, array $content, $contentType='application/x-www-form-urlencoded', $encodeURL = false ) {		
 		return $this->send( 'GET', $url, $content, $contentType, $encodeURL );
-	}	
+	}
 	
 	/**
-	 * Send a request.
+	 * Send a HTTP PUT request. This a convenience method for #send().
+	 *
+	 * @see #send().
+	 */	
+	function put( $url, array $content, $contentType='application/x-www-form-urlencoded', $encodeURL = false ) {		
+		return $this->send( 'PUT', $url, $content, $contentType, $encodeURL );
+	}
+	
+	/**
+	 * Send a HTTP DELETE request. This a convenience method for #send().
+	 *
+	 * @see #send().
+	 */	
+	function delete( $url, array $content, $contentType='application/x-www-form-urlencoded', $encodeURL = false ) {		
+		return $this->send( 'DELETE', $url, $content, $contentType, $encodeURL );
+	}
+	
+	/**
+	 * Send a HTTP PATCH request. This a convenience method for #send().
+	 *
+	 * @see #send().
+	 */	
+	function patch( $url, array $content, $contentType='application/x-www-form-urlencoded', $encodeURL = false ) {		
+		return $this->send( 'PATCH', $url, $content, $contentType, $encodeURL );
+	}
+
+	/**
+	 * Send a HTTP OPTIONS request. This a convenience method for #send().
+	 *
+	 * @see #send().
+	 */	
+	function options( $url, array $content, $contentType='application/x-www-form-urlencoded', $encodeURL = false ) {		
+		return $this->send( 'OPTIONS', $url, $content, $contentType, $encodeURL );
+	}
+	
+	/**
+	 * Send a HTTP request.
 	 *
 	 * @param url		the target url
 	 * @param content	the array with the content to send. Example: array( 'name' => 'Bob' )
@@ -58,14 +82,14 @@ class HttpSender {
 	 */
 	function send( $method, $url, array $content, $contentType, $encodeURL ) {
 		$options = array(
-			'http' => array( // also works for https
+			'http' => array( // also works with https
 				'header'  => 'Content-type: '. $contentType,
 				'method'  => $method,
 				'content' => http_build_query( $content )
 			),
 		);
 		$context = stream_context_create( $options );
-		$targetURL = ( $encodeURL ) ? urlencode( $url ) : $url;
+		$targetURL = $encodeURL ? urlencode( $url ) : $url;
 		return file_get_contents( $targetURL , false, $context );
 	}
 	
